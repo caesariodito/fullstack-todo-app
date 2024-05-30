b41b56597a42b5bcc3ec7fdf18449a8f019b013b
Caesario Dito <caesariodito@gmail.com>
2024-05-31 06:14:06 +0700

    init: spring boot backend init

---
+++ be-todo/src/main/java/com/sesar/betodo/BeTodoApplication.java
@@ -0,0 +1,13 @@
+package com.sesar.betodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeTodoApplication.class, args);
+	}
+
+}
---
+++ be-todo/src/test/java/com/sesar/betodo/BeTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesar.betodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
