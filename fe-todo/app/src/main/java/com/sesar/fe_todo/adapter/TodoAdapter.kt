32fced995a227627281a65d3314ebe9330526d15
Caesario Dito <caesariodito@gmail.com>
2024-06-01 17:18:26 +0700

    feat: add view binding; adjust adapter; new ui;

--- fe-todo/app/src/main/java/com/sesar/fe_todo/adapter/TodoAdapter.kt
+++ fe-todo/app/src/main/java/com/sesar/fe_todo/adapter/TodoAdapter.kt
@@ -7,29 +7,36 @@ import androidx.recyclerview.widget.RecyclerView
 import com.sesar.fe_todo.databinding.ItemTodoBinding
 import com.sesar.fe_todo.model.Todo
 
-class TodoAdapter : RecyclerView.Adapter<TodoAdapter.ToDoViewHolder>()  {
+class TodoAdapter(
+    private val onEdit: (Todo) -> Unit,
+    private val onDelete: (Todo) -> Unit
+) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>()  {
 
-    private var todos: List<Todo> = listOf()
-
-    inner class ToDoViewHolder(val binding: ItemTodoBinding) : RecyclerView.ViewHolder(binding.root)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val binding = ItemTodoBinding.inflate(layoutInflater, parent, false)
-        return ToDoViewHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: ToDoViewHolder, position: Int) {
-        val todo = todos[position]
-        holder.binding.todo = todo
-        holder.binding.executePendingBindings()
-    }
-
-    override fun getItemCount(): Int = todos.size
+    private var todos = listOf<Todo>()
 
     @SuppressLint("NotifyDataSetChanged")
     fun setTodos(todos: List<Todo>) {
         this.todos = todos
         notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val binding = ItemTodoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TodoViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder.bind(todos[position])
+    }
+
+    override fun getItemCount(): Int = todos.size
+
+    inner class TodoViewHolder(private val binding: ItemTodoBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(todo: Todo) {
+            binding.todo = todo
+            binding.executePendingBindings()
+            binding.editButton.setOnClickListener { onEdit(todo) }
+            binding.deleteButton.setOnClickListener { onDelete(todo) }
+        }
     }
 }
--- fe-todo/app/src/main/java/com/sesar/fe_todo/view/MainActivity.kt
+++ fe-todo/app/src/main/java/com/sesar/fe_todo/view/MainActivity.kt
@@ -2,14 +2,17 @@ package com.sesar.fe_todo.view
 
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sesar.fe_todo.R
 import com.sesar.fe_todo.databinding.ActivityMainBinding
+import com.sesar.fe_todo.databinding.DialogEditTodoBinding
 import com.sesar.fe_todo.viewmodel.TodoViewModel
 import com.sesar.fe_todo.adapter.TodoAdapter
+import com.sesar.fe_todo.model.Todo
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,8 +39,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupRecyclerView() {
-        adapter = TodoAdapter()
+        adapter = TodoAdapter(onEdit = { todo -> showEditDialog(todo) }, onDelete = { todo -> viewModel.deleteTodo(todo.id) })
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun showEditDialog(todo: Todo) {
+        val dialogBinding = DialogEditTodoBinding.inflate(layoutInflater)
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Edit Todo")
+            .setView(dialogBinding.root)
+            .setPositiveButton("Save") { _, _ ->
+                val updatedTodo = todo.copy(
+                    title = dialogBinding.editTextTitle.text.toString(),
+                    description = dialogBinding.editTextDescription.text.toString()
+                )
+                viewModel.updateTodo(todo.id, updatedTodo)
+            }
+            .setNegativeButton("Cancel", null)
+            .create()
+        dialogBinding.editTextTitle.setText(todo.title)
+        dialogBinding.editTextDescription.setText(todo.description)
+        dialog.show()
     }
 }
